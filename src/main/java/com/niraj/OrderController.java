c2643ceeb210653c5845ff2e9120c333921eca8f
Niraj <niraj.webtech@gmail.com>
2023-01-09 15:13:35 +0530

    Welcome Route

--- src/main/java/com/niraj/OrderController.java
+++ src/main/java/com/niraj/OrderController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class OrderController {
 
+	@GetMapping("/")
+	public String welcomeRoute() {
+		return "Welcome to <h1>Havells Project</h1>";
+	}
 	
 	@GetMapping("/orders")
 	public String orderList() {
