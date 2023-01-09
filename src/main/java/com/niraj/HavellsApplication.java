ae1afdae172593d36f640d82e57d2988283ab2b1
Niraj <niraj.webtech@gmail.com>
2023-01-09 14:46:25 +0530

    Initial commit for GITHUN using Terminal

---
+++ src/main/java/com/niraj/HavellsApplication.java
@@ -0,0 +1,14 @@
+package com.niraj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HavellsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HavellsApplication.class, args);
+		System.out.println("||||||===> Spring Boot Project HAVELLS START ");
+	}
+
+}
